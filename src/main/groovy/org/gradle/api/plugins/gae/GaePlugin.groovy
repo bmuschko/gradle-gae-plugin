67a7de130e11be59261eb053351db54eb29e502d
Benjamin Muschko <benjamin.muschko@gmail.com>
2012-12-09 07:50:25 -0500

    Issue #35: Exposed noCookies and oauth2 convention properties.

--- src/main/groovy/org/gradle/api/plugins/gae/GaePlugin.groovy
+++ src/main/groovy/org/gradle/api/plugins/gae/GaePlugin.groovy
@@ -188,6 +188,7 @@ class GaePlugin implements Plugin<Project> {
             gaeAppConfigTaskTemplate.conventionMapping.map('email') { gaePluginConvention.appCfg.email }
             gaeAppConfigTaskTemplate.conventionMapping.map('server') { gaePluginConvention.appCfg.server }
             gaeAppConfigTaskTemplate.conventionMapping.map('host') { gaePluginConvention.appCfg.host }
+            gaeAppConfigTaskTemplate.conventionMapping.map('noCookies') { gaePluginConvention.appCfg.noCookies }
             gaeAppConfigTaskTemplate.conventionMapping.map('passIn') { gaePluginConvention.appCfg.passIn }
             gaeAppConfigTaskTemplate.conventionMapping.map('password') {
                 // Password from gradle.properties takes precedence
@@ -195,6 +196,7 @@ class GaePlugin implements Plugin<Project> {
             }
             gaeAppConfigTaskTemplate.conventionMapping.map('httpProxy') { gaePluginConvention.appCfg.httpProxy }
             gaeAppConfigTaskTemplate.conventionMapping.map('httpsProxy') { gaePluginConvention.appCfg.httpsProxy }
+            gaeAppConfigTaskTemplate.conventionMapping.map('oauth2') { gaePluginConvention.appCfg.oauth2 }
         }
     }
 
